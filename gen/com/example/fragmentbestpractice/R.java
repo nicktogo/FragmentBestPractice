5c15cd1d725dbd56700a1c63002cfe06846bc64a
Nick Lee <gnick1995@gmail.com>
2015-02-08 23:53:04 +0800

    Done

--- gen/com/example/fragmentbestpractice/R.java
+++ gen/com/example/fragmentbestpractice/R.java
@@ -27,15 +27,21 @@ public final class R {
         public static final int spilt_title_vertical=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int news_content=0x7f080002;
-        public static final int news_title=0x7f080001;
-        public static final int visibility_layout=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int news_content=0x7f080005;
+        public static final int news_content_fragment=0x7f080002;
+        public static final int news_content_layout=0x7f080001;
+        public static final int news_title=0x7f080004;
+        public static final int news_title_fragment=0x7f080000;
+        public static final int news_title_list_view=0x7f080006;
+        public static final int visibility_layout=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int news_content_frag=0x7f030001;
-        public static final int news_item=0x7f030002;
+        public static final int news_content=0x7f030001;
+        public static final int news_content_frag=0x7f030002;
+        public static final int news_item=0x7f030003;
+        public static final int news_title_frag=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
